17d3abe0960cd910adcaa9862df4db6a561b5a7c
hugosenari <hugosenari@gmail.com>
2022-03-12 01:58:55 -0300

    chore: re index edn

---
+++ 0_2_3/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''EDN and Clojure parser'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-edn-0_2_3.flake = false;
+  inputs.src-edn-0_2_3.owner = "rosado";
+  inputs.src-edn-0_2_3.ref   = "refs/tags/0.2.3";
+  inputs.src-edn-0_2_3.repo  = "edn.nim";
+  inputs.src-edn-0_2_3.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-edn-0_2_3"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-edn-0_2_3";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
