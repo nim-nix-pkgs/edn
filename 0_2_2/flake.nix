a4a4f4fc59c30b139e05c89797a6fd0ce1d3fbe7
hugosenari <hugosenari@gmail.com>
2022-03-27 21:49:16 -0300

    chore: re index edn

--- 0_2_2/flake.nix
+++ 0_2_2/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-edn-0_2_2.ref   = "refs/tags/0.2.2";
   inputs.src-edn-0_2_2.owner = "rosado";
   inputs.src-edn-0_2_2.repo  = "edn.nim";
-  inputs.src-edn-0_2_2.dir   = "";
   inputs.src-edn-0_2_2.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
